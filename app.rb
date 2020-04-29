b1ff7eaa77c253a34ac6b0d08d32e6555c57ebe0
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-29 00:04:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,16 @@ class App < Sinatra::Base
     erb :create_puppy
   end
   
+  get '/create' do 
+        @name =  params["name"]
+    @coach = params["coach"]
+    @pg =    params["pg"]
+    @sg =    params["sg"]
+    @pf =    params["pf"]
+    @sf =    params["sf"]
+    @c =     params["c"]
+  end
+  
   post '/puppy' do 
     erb :display_puppy
   end
